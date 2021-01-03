552522a5373dfa25d9d958969ae88147c1fe6011
Quentin McGaw <quentin.mcgaw@gmail.com>
2021-01-03 05:25:21 +0000

    Linter

--- internal/golang/translate.go
+++ internal/golang/translate.go
@@ -4,8 +4,7 @@ import "github.com/qdm12/xcputranslate/internal/docker"
 
 func Translate(dockerPlatform docker.Platform) (arch, arm string) {
 	arch = string(dockerPlatform.Arch)
-	switch dockerPlatform.Arch {
-	case docker.ARM:
+	if dockerPlatform.Arch == docker.ARM {
 		switch dockerPlatform.Version {
 		case docker.V6:
 			arm = "6"
